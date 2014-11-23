f465e3e73f434155ffab40152f4f186a09a2698b
John Doe <johnny@newmail.ru>
2014-11-23 21:15:52 +0300

    Added watch to default task

--- gulpfile.js
+++ gulpfile.js
@@ -46,5 +46,5 @@ gulp.task('watch', function(){
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
 
