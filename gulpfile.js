dbdb22199a601b2948b18653f58c0ba8d8f088bd
John Doe <johnny@newmail.ru>
2014-11-24 09:02:44 +0300

    Added gulpif and minified production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
 	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
 	gconcat = require('gulp-concat');
 
 var env,
@@ -49,6 +51,7 @@ gulp.task('js', function (){
 	gulp.src(jsSources)
 		.pipe(gconcat('script.js'))
 		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
